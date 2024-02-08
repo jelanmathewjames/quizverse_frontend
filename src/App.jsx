d81f51c75d66ade77b557f8b9780270eb6412f67
SUJITH T S <tssujith2002@gmail.com>
2024-02-08 05:42:46 +0530

    added route for homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import  Footer  from "./components/Footer"
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
 
 
 function App() {
   
   return (
-    <>
-      <Footer />
-    </>
+    <Routes>
+      <Route path="/" element={<Home/>}> </Route>
+    </Routes>
   )
 }
 
