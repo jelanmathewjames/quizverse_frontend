6526e8410cbfc5397e78185e2905782440994557
SUJITH T S <tssujith2002@gmail.com>
2024-02-08 05:40:40 +0530

    Added  Home page.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HomeLayout from "../layouts/HomeLayout";
+
+const Home = () => {
+  return (
+    <HomeLayout>
+      <div>Home</div>
+    </HomeLayout>
+  );
+};
+
+export default Home;
