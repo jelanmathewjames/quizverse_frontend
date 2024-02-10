34f7e2717c0a12066e432d8eaed87ed57478b1e0
SUJITH T S <tssujith2002@gmail.com>
2024-02-10 23:01:35 +0530

    removed redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,12 @@
 import ReactDOM from "react-dom/client";
 import { Toaster } from "react-hot-toast";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
 
 import App from "./App.jsx";
-import store from "./redux/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
     <BrowserRouter>
       <App />
       <Toaster />
     </BrowserRouter>
-  </Provider>
 );
