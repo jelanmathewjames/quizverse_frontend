0392c36f5fa70329cb81bb5a0b8ffbf3dc38055e
SUJITH T S <tssujith2002@gmail.com>
2024-02-17 15:47:54 +0530

    Refactor CSS class to className

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,9 +6,9 @@ const Home = () => {
   return (
     <HomeLayout>
       <div className=" relative overflow-hidden dark:bg-gradient-to-l dark:from-slate-800 dark:to-slate-950 ">
-          <div class="absolute opacity-50 animate-blob dark:mix-blend-overlay mix-blend-multiply filter blur-xl top-1/2 left-1/3 transform translate-x-1/2 translate-y-1/2 w-80 h-80 bg-yellow-400 rounded-full "></div>
-          <div class="absolute opacity-50 animate-blob animation-delay-2000 dark:mix-blend-overlay mix-blend-multiply filter blur-xl top-1/3 left-1/2 transform translate-x-1/2 translate-y-1/2 w-72 h-72 bg-blue-500 rounded-full "></div>
-          <div class="absolute opacity-50 animate-blob animation-delay-4000 dark:mix-blend-overlay mix-blend-multiply filter blur-xl top-1/3 left-1/6 sm:left-1/3 transform translate-x-1/2 translate-y-1/2 w-72 h-72 bg-pink-400 rounded-full "></div> 
+          <div className="absolute opacity-50 animate-blob dark:mix-blend-overlay mix-blend-multiply filter blur-xl top-1/2 left-1/3 transform translate-x-1/2 translate-y-1/2 w-80 h-80 bg-yellow-400 rounded-full "></div>
+          <div className="absolute opacity-50 animate-blob animation-delay-2000 dark:mix-blend-overlay mix-blend-multiply filter blur-xl top-1/3 left-1/2 transform translate-x-1/2 translate-y-1/2 w-72 h-72 bg-blue-500 rounded-full "></div>
+          <div className="absolute opacity-50 animate-blob animation-delay-4000 dark:mix-blend-overlay mix-blend-multiply filter blur-xl top-1/3 left-1/6 sm:left-1/3 transform translate-x-1/2 translate-y-1/2 w-72 h-72 bg-pink-400 rounded-full "></div> 
         <div className="relative pt-10 flex items-center justify-center gap-10 mx-16 h-[100vh]">
           <div className=" space-y-6">
             <h1 className="text-5xl font-semibold text-center">
