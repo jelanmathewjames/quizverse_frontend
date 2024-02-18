93b1a7d33656bd74191144123bacb662ff9c4d89
SUJITH T S <tssujith2002@gmail.com>
2024-02-18 12:56:22 +0530

    Update page styles

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -50,17 +50,23 @@ const Signin = () => {
   };
   return (
     <HomeLayout>
-      <div className="flex items-center justify-center h-[100vh] ">
+      <div className="flex items-center  justify-center h-[100vh] relative overflow-hidden ">
+      <div className="absolute opacity-40 animate-blob dark:mix-blend-overlay mix-blend-multiply filter blur-xl top-1/2 left-1/3 transform translate-x-1/2 translate-y-1/2 w-80 h-80 bg-yellow-400 rounded-full "></div>
+          <div className="absolute opacity-40 animate-blob animation-delay-2000 dark:mix-blend-overlay mix-blend-multiply filter blur-xl top-1/3 left-1/2 transform translate-x-1/2 translate-y-1/2 w-72 h-72 bg-blue-500 rounded-full "></div>
+          <div className="absolute opacity-40 animate-blob animation-delay-4000 dark:mix-blend-overlay mix-blend-multiply filter blur-xl top-1/3 left-1/6 sm:left-1/3 transform translate-x-1/2 translate-y-1/2 w-72 h-72 bg-pink-400 rounded-full "></div> 
+
         <form
           onSubmit={onFormSubmit}
           noValidate
-          className="flex flex-col justify-center gap-3 rounded-lg p-10 bg-gray-900 border-2  backdrop-blur-md "
+          className="flex flex-col justify-center gap-3 rounded-lg border-[1px]  p-10 backdrop-blur-md shadow-2xl 
+          w-[80vw] sm:w-[410px] 
+          "
         >
           <h1 className="text-2xl text-center font-bold pb-3">SignIn</h1>
 
           <label className="form-control w-full max-w-xs">
             <div className="label">
-              <span    className="label-text text-white font-bold">user name</span>
+              <span    className="label-text font-bold">user name</span>
             </div>
             <input
              onChange={handleUserInputs}
@@ -76,7 +82,7 @@ const Signin = () => {
             <div className="label">
               <span
                 htmlFor="password"
-                className="label-text text-white font-bold"
+                className="label-text  font-bold"
               >
                 Password
               </span>
@@ -95,17 +101,18 @@ const Signin = () => {
               <span className="label-text-alt">
                 <Link
                   to="/resetPassword"
-                  className="cursor-pointer text-accent"
+                  className="cursor-pointer "
+
                 >
                   Forgot password ?
                 </Link>
               </span>
             </div>
           </label>
-          <button className="btn btn-outline btn-info">Login</button>
+          <button className="btn btn-outline btn-active">Login</button>
           <p className="text-center">
             Donot have an account ?{" "}
-            <Link to="/signup" className="cursor-pointer text-accent">
+            <Link to="/signup" className="cursor-pointer ">
               Signup
             </Link>
           </p>
