89da8646a7e0a9258272c3a7039ce3a93f5b342b
SUJITH T S <tssujith2002@gmail.com>
2024-02-24 13:01:01 +0530

    Add AuthProvider to App render

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import { Toaster } from "react-hot-toast";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
+import { AuthProvider } from "./context/AuthProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
+    <AuthProvider>
       <App />
+    </AuthProvider>
       <Toaster />
     </BrowserRouter>
 );
