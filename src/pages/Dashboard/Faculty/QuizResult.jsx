1b678b1f7c1b990ba6adccc22f17ace9cbcf139d
SUJITH T S <tssujith2002@gmail.com>
2024-04-18 19:56:35 +0530

    feat: display quiz details and student results

--- src/pages/Dashboard/Faculty/QuizResult.jsx
+++ src/pages/Dashboard/Faculty/QuizResult.jsx
@@ -1,11 +1,98 @@
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 
-const QuizResult = ({quizId}) => {
+import quizResultData from './dummy_data/quizResult.json';
+const QuizResult = ({ quizId }) => {
+    const [quizData,setQuizData] = useState({});
+    useEffect(() => {   
+        setQuizData(quizResultData);
+    }, []);
+    
+    const quiz = quizData.quiz;
+    const results = quizData.results;
+
     return (
-        <div>
-            quizId : {quizId}
-            display list of students who took the quiz with mark .
-            if quiz not completed then display quiz is not completed , also display the expire time for quiz.
+        <>
+     
+        
+        <div className="container mx-auto px-4">
+               
+ 
+    <div className="w-full">
+        <div className="bg-base-100 rounded-lg shadow-lg mb-5">
+            <table className="table-fixed border-collapse w-full">
+             
+                <tbody>
+                    <tr>
+                        <td className="border px-4 py-2 font-bold text-center text-lg bg-base-200" colSpan={2}>{quiz.title}</td>
+                    </tr>
+                    {/* <tr>
+                        <th className="border px-4 py-2 text-left">Quiz ID</th>
+                        <td className="border px-4 py-2">{quiz.quizId}</td>
+                    </tr> */}
+                    <tr>
+                        <th className="border px-4 py-2 text-left">Department</th>
+                        <td className="border px-4 py-2">{quiz.department}</td>
+                    </tr>
+                    <tr>
+                        <th className="border px-4 py-2 text-left">Subject</th>
+                        <td className="border px-4 py-2">{quiz.subject}</td>
+                    </tr>
+                    <tr>
+                        <th className="border px-4 py-2 text-left">Semester</th>
+                        <td className="border px-4 py-2">{quiz.semester}</td>
+                    </tr>
+                
+                    <tr>
+                        <th className="border px-4 py-2 text-left">Total Questions</th>
+                        <td className="border px-4 py-2">{quiz.totalQuestions}</td>
+                    </tr>
+                    <tr>
+                        <th className="border px-4 py-2 text-left">Total Marks</th>
+                        <td className="border px-4 py-2">{quiz.totalMarks}</td>
+                    </tr>
+                    
+                    
+                    <tr>
+                        <th className="border px-4 py-2 text-left">Date</th>
+                        <td className="border px-4 py-2">{quiz.date}</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
+    </div>
+
+
+
+    <div className="overflow-x-auto shadow-2xl ">
+        <table className="table-auto w-full   ">
+            <thead>
+                <tr className="bg-base-300"></tr>
+                <tr className="bg-base-300 ">
+                    <th className="px-4 border  border-gray-400 py-2 font-bold">Student Name</th>
+                    <th className="px-4 border border-gray-400 py-2 font-bold">Score</th>
+                </tr>
+            </thead>
+            {/* Data Rows */}
+            <tbody>
+                {results && results.map((result, index) => (
+                    <tr key={index} className={index % 2 === 0 ? '' : 'bg-base-100'}>
+                        <td className="border border-gray-400 px-4 py-2">{ result.score == -1 ? <span className='text-red-400'>{result.studentName}</span> : result.studentName}</td>
+                        <td className="border border-gray-400 px-4 py-2 text-right">{ result.score == -1 ? <span className='text-red-400'>AB</span> : result.score }</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    </div>
+    <div className='flex justify-center'> 
+    <button className="btn btn-outline btn-active mt-3 " >
+                  export to excel
+              </button>
+    </div>
+    
+</div>
+</>
+    
     );
 }
 
