99401994afe4ca21115c9dd37cd468891e94f587
jelanmathewjames <jelanmathewjames1234@gmail.com>
2024-04-30 21:47:44 +0530

    new backend base url

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8000/api/v1";
+const BASE_URL = "https://quizverse.azurewebsites.net/api/v1";
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
