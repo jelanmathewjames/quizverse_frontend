202e3b48c5d48aa8e82d4391de3ec4df5d516ff6
jelanmathewjames <jelanmathewjames1234@gmail.com>
2024-05-06 16:02:05 +0530

    adding refresh login logout function to the context

--- src/components/PersistLogin.jsx
+++ src/components/PersistLogin.jsx
@@ -1,13 +1,12 @@
 import { Outlet } from "react-router-dom"
 import { useState, useEffect } from "react";
-import useRefreshToken from '../hooks/useRefreshToken'
+
 import useAuth from '../hooks/useAuth'
 
 const PersistLogin = () => {
     const [isLoading, setIsLoading] = useState(true);
-    const refresh = useRefreshToken();
-    const { auth, persist } = useAuth();
-
+    const { auth, persist, refresh } = useAuth();
+    
     useEffect(() => {
         let isMounted = true;
         const verifyRefreshToken = async () => {
