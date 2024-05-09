6b05ba1bc666d6b74d61e5431542f3e062578e4e
SUJITH T S <tssujith2002@gmail.com>
2024-05-09 15:48:02 +0530

    add admin functionalities

--- src/pages/Dashboard/Admin/index.jsx
+++ src/pages/Dashboard/Admin/index.jsx
@@ -1,17 +1,19 @@
 import DashboardLayout from '../../../components/DashboardLayout';
 import Profile from '../../../components/Profile';
 import items  from './adminNavItems.json';
+import CreateCommunity from './CreateCommunity';
 import CreateCommunityAdmin from './CreateCommunityAdmin';
 import CreateCourse from './CreateCourse';
 import CreateInstitute from './CreateInstitute';
 import CreateInstituteAdmin from './CreateInstituteAdmin';
+import GiveRole from './GiveRole';
 const AdminDashboard = () => {
   return (
     <DashboardLayout navitems={items} title="Admin">
-      <CreateCommunityAdmin />
-      <CreateCourse />
+      <GiveRole />
       <CreateInstitute />
-      <CreateInstituteAdmin />
+      <CreateCommunity/>
+      <CreateCourse />
     </DashboardLayout>
   );
 };
