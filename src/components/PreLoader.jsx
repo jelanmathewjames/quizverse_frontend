29e9003f59890fc104d638a0510acfa4fb1ad8c3
SUJITH T S <tssujith2002@gmail.com>
2024-05-11 08:53:13 +0530

    feat: Add Preloader component for better user experience

---
+++ src/components/PreLoader.jsx
@@ -0,0 +1,14 @@
+import useTheme from "../hooks/useTheme";
+const Preloader = () => {
+useTheme();
+    return (    
+    <div className="loader">
+        <div className="inner one"></div>
+        <div className="inner two"></div>
+        <div className="inner three"></div>
+      </div>
+    );
+  };
+  
+  export default Preloader;
+  
