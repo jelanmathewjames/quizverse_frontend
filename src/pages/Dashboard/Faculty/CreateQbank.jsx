480d199d585b800933faf3b2fd9b8c51238c0af3
SUJITH T S <tssujith2002@gmail.com>
2024-05-12 02:43:37 +0530

    refactor: Remove unused code and optimize ModuleQuestionReader component

--- src/pages/Dashboard/Faculty/CreateQbank.jsx
+++ src/pages/Dashboard/Faculty/CreateQbank.jsx
@@ -1,41 +1,25 @@
-import axios from "axios";
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
 import { axiosPrivate } from "../../../config/axiosInstance";
-import departmentsData from "../Faculty/dummy_data/departments.json";
-import moduleData from "../Faculty/dummy_data/module.json";
-import semestersData from "../Faculty/dummy_data/semesters.json";
-import subjectsData from "../Faculty/dummy_data/subjects.json";
+import { useLocalStorage } from "../../../hooks/useLocalStorage";
 import { containerVariants } from "./../../../helpers/animationHelpers/containerVariants";
-import ModuleQuestionReader from "./ModuleQuestionReader";
+import ModuleQuestionReadWrite from "./ModuleQuestionReadWrite";
 
 const CreateQubank = () => {
-  const [subjects, setSubjects] = useState("");
   const [modules, setModules] = useState([]);
-
-  const [selectedSubject, setSelectedSubject] = useState("");
-  const [questionData, setQuestionData] = useState([]);
-
+  const [quizId, setQuziId] = useState("");
+  const [selectedCourseId, setSelectedCourseId] = useState();
+  // store quizId in local storage
   const [title, setTitle] = useState("");
   const [selectedCourse, setSelectedCourse] = useState("");
   const [courseList, setCourseList] = useState([]);
 
-  // const modules = moduleData.filter((module) => module.course_id == selectedSubject);
+  const { setItem: setQuizIdInLocalStorage } = useLocalStorage("quizId");
+  const { setItem: setCourseInLocalStorage } = useLocalStorage("courseId");
+
   const [loading, setLoading] = useState(false);
-
-  const handleQuestionData = (moduleId, data) => {
-    setQuestionData((prevData) => ({
-      ...prevData,
-      [moduleId]: {
-        ...prevData[moduleId],
-        ...data,
-      },
-    }));
-    console.log(questionData);
-  };
-
   //fetch course from server
   useEffect(() => {
     const fetchCourse = async () => {
@@ -49,42 +33,7 @@ const CreateQubank = () => {
     fetchCourse();
   }, []);
 
-  // //fetch modules from server
-  //   useEffect(() => {
-  //     const fetchModules = async () => {
-  //         try {
-  //             const response = await axios.get('/api/modules', {
-  //               id: selectedSubject
-  //             });
-  //             setModules(response.data);
-  //         } catch (error) {
-  //             console.error('Failed to fetch modules:', error);
-  //         }
-  //     };
-  //     fetchModules();
-  // }, [selectedSubject]);
 
-  const CreateQBank = async () => {
-    //   if (Object.keys(questionData).length !== modules.length)
-    //   { toast.error("Please complete all module questions");
-    //     return;
-    //  }
-    //   try {
-    //     setLoading(true);
-    //       const response = await axios.post("/api/createQbank", {
-    //         department: selectedDepartment,
-    //         semester: selectedSemester,
-    //         subject: selectedSubject,
-    //         questions: questionData,
-    //       });
-    //       toast.success("QuizBank created successfully");
-    //       console.log(response.data);
-    //   } catch (error) {
-    //     toast.error("Failed to Create QuizBank");
-    //   } finally {
-    //     setLoading(false);
-    //   }
-  };
 
   const CreateQuestionBank = async () => {
     if (!title || !selectedCourse) {
@@ -93,63 +42,91 @@ const CreateQubank = () => {
     }
     try {
       setLoading(true);
-      const response = await axiosPrivate.post("/faculty/questionbank", {
+      const response = await axiosPrivate.post("/quiz/qbank/", {
         title: title,
         course_id: selectedCourse,
       });
       toast.success("QuestionBank created successfully");
-      console.log(response.data);
+      setQuziId(response.data.id); // store quizId in state
+      console.log("selectedCourse", selectedCourse);
+      setSelectedCourseId(selectedCourse); // store courseId in state
+      setQuizIdInLocalStorage(response.data.id); // store quizId in local storage , quizId as key
+      setCourseInLocalStorage(selectedCourse); // store courseId in local storage ,courseId as key
     } catch (error) {
-      toast.error("Failed to Create QuestionBank");
+      toast.error(error.response.data.message);
     } finally {
+      // setSelectedCourse("");
+      setTitle("");
       setLoading(false);
     }
-  }
+  };
+
+  // fetch modules from server based on the selected course
+  useEffect(() => {
+    const fetchModules = async () => {
+      try {
+        const response = await axiosPrivate.get(
+          "/admin/module?id=" + selectedCourseId
+        );
+        setModules(response.data);
+        console.log("modules", modules);
+      } catch (error) {
+        console.error("Failed to fetch modules:", error);
+      }
+    };
+    if (selectedCourseId) {
+      fetchModules();
+    }
+  }, [selectedCourseId]);
+
+  useEffect(() => {
+    if (quizId) {
+      console.log("currentQuiz id : " + quizId);
+    }
+  }, [quizId]);
 
   return (
     <div className="flex flex-col gap-5">
       {/* div for input:title,coures,quizBankCreatebtn */}
-     
+
       <motion.div
         className="flex flex-col gap-3  items-center md:items-start w-full "
         initial="hidden"
         animate="visible"
         variants={containerVariants}
       >
-       <h1 className="text-2xl font-bold pb-5">Create QuizBank</h1>
-      <div className="inputwrapper flex flex-col items-center md:flex-row gap-3 w-full lg:w-1/2">
-        {/* input: quiz title */}
-        <input
-          type="text"
-          placeholder="Type Quiz Title.."
-          value={title}
-          onChange={(event) => setTitle(event.target.value)}
-          className="input input-bordered w-[80%] md:w-[60%] "
-        />
-        {/* fetch course using api  */}
-        <select
-          className="select select-bordered   w-[80%] md:w-[60%]"
-          value={selectedCourse}
-          onChange={(event) => setSelectedCourse(event.target.value)}
-        >
-          <option disabled value="">
-            Select Course
-          </option>
-          {courseList.length != 0 ? (
-            courseList.map((course) => (
-              <option key={course.id} value={course.id}>
-                {course.name} {course.code}
-              </option>
-            ))
-          ) : (
+        <h1 className="text-2xl font-bold pb-5">Create QuizBank</h1>
+        <div className="inputwrapper flex flex-col items-center md:flex-row gap-3 w-full lg:w-1/2">
+          {/* input: quiz title */}
+          <input
+            type="text"
+            placeholder="Type Quiz Title.."
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+            className="input input-bordered w-[80%] md:w-[60%] "
+          />
+          {/* fetch course using api  */}
+          <select
+            className="select select-bordered   w-[80%] md:w-[60%]"
+            value={selectedCourse}
+            onChange={(event) => setSelectedCourse(event.target.value)}
+          >
             <option disabled value="">
-              No Course Found
+              Select Course
             </option>
-          )}
-        </select>
-      </div>
-        
-          
+            {courseList.length != 0 ? (
+              courseList.map((course) => (
+                <option key={course.id} value={course.id}>
+                  {course.name} {course.code}
+                </option>
+              ))
+            ) : (
+              <option disabled value="">
+                No Course Found
+              </option>
+            )}
+          </select>
+        </div>
 
         <button
           className="btn btn-outline btn-active mt-3 w-[80%] md:max-w-xs"
@@ -160,50 +137,33 @@ const CreateQubank = () => {
           ) : (
             "Create QuestionBank"
           )}
-
         </button>
       </motion.div>
       {/* ------------------------------------------- */}
-      <div className="col-span-2 h-80 bg-gray-500">Child 2</div>
-      {/* {selectedSubject && (
+      {/* module wise question read and write to the backend */}
+      {quizId && (
         <motion.div
           variants={containerVariants}
           initial="hidden"
           animate="visible"
         >
-          {console.log("inside the modules")}
-          {modules
-            .filter((module) => module.course_id == selectedSubject)
-            .map((module, index) => (
-              <ModuleQuestionReader
+          {modules.length != 0 ? (
+            modules.map((module, index) => (
+              <ModuleQuestionReadWrite
                 key={index} // It's important to provide a unique key for each child in a list
-                moduleName={module.module_name}
-                moduleNumber={module.module_num}
-                moduleId={module.module_id}
-                onQuestionData={(data) =>
-                  handleQuestionData(module.module_id, data)
-                }
-                questionType={"mcq"}
+                module_name={module.module_name}
+                module_number={module.module_number}
+                module_id={module.id}
+                quiz_id={quizId}
+                question_number= {index*10} 
+                
               />
-            ))}
-          {modules.length === 0 ? (
-            <div className="flex justify-center p-5">No modules found</div>
+            ))
           ) : (
-            <div className="flex justify-center p-">
-              <button
-                className="btn btn-outline btn-active mt-3"
-                onClick={CreateQBank}
-              >
-                {loading ? (
-                  <div className="animate-spin rounded-full  w-5 h-5 border-t-2 border-b-3 dark:border-[black] border-[#ffffff]"></div>
-                ) : (
-                  "Create QuestionBank"
-                )}
-              </button>
-            </div>
+            <div className="flex justify-center p-5">No modules found</div>
           )}
         </motion.div>
-      )} */}
+      )}
     </div>
   );
 };
---
+++ src/pages/Dashboard/Faculty/ModuleQuestionReadWrite.jsx
@@ -0,0 +1,224 @@
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+
+import { axiosPrivate } from "../../../config/axiosInstance";
+
+const ModuleQuestionReadWrite = ({
+    module_name,
+    module_number,
+    module_id,
+    quiz_id,
+    question_number,
+  }) => {
+  
+   const [questionNumber, setQuestionNumber] = useState(question_number + 2);
+  const [questions, setQuestions] = useState([
+    {
+      question: "",
+      question_number: questionNumber -2,
+      options: [
+        { option_number: "A", option_value: "", is_correct: false },
+        { option_number: "B", option_value: "", is_correct: false },
+        { option_number: "C", option_value: "", is_correct: false },
+        { option_number: "D", option_value: "", is_correct: false },
+      ],
+      module_id: module_id,
+      question_type: "MCQ"
+    },
+
+    {
+      question: "",
+      question_number: questionNumber -1,
+      options: [
+        { option_number: "A", option_value: "", is_correct: false },
+        { option_number: "B", option_value: "", is_correct: false },
+        { option_number: "C", option_value: "", is_correct: false },
+        { option_number: "D", option_value: "", is_correct: false },
+      ],
+      module_id: module_id,
+      question_type: "MCQ"
+    },
+  ]);
+  // setLastQuestionNumber(prevLastQuestionNumber => prevLastQuestionNumber + 2);
+  const addQuestion = () => {
+   setQuestionNumber(questionNumber + 1);
+    setQuestions([
+      ...questions,
+      {
+        question: "",
+        question_number:questionNumber,
+        options: [
+          { option_number: "A", option_value: "", is_correct: false },
+          { option_number: "B", option_value: "", is_correct: false },
+          { option_number: "C", option_value: "", is_correct: false },
+          { option_number: "D", option_value: "", is_correct: false },
+        ],
+        module_id: module_id,
+        question_type: "MCQ"
+      },
+    ]);
+  };
+  const updateQuestion = (index, field, value) => {
+    const newQuestions = [...questions];
+    if (field === "question") {
+      newQuestions[index].question = value;
+    } else if (field === "options") {
+      if ('optionValue' in value) {
+        newQuestions[index].options[value.optionIndex].option_value = value.optionValue;
+      }
+      if ('isCorrect' in value) {
+        newQuestions[index].options[value.optionIndex].is_correct = value.isCorrect;
+      }
+    }
+    setQuestions(newQuestions);
+    console.log(questions);
+  };
+
+  const [isSaving, setIsSaving] = useState(false);
+  const checkIfEveryQuestionHasCorrectOption = (questions) => {
+    return questions.every(question => question.options.some(option => option.is_correct));
+  }
+  const checkIfEveryOptionHasValue = (questions) => {
+    return questions.every(question => 
+      question.options.every(option => option.option_value.trim() !== "")
+    )
+  }
+  const validateQuestions = () => {
+    for (const question of questions) {
+      if (question.question.trim() === ""){
+        toast.error("Please fill in all question fields.");
+        return false;
+      }
+      if(!checkIfEveryQuestionHasCorrectOption(questions)){
+        toast.error("Please select the correct option for each question.");
+        return false;
+      }
+      if(!checkIfEveryOptionHasValue(questions)){
+        toast.error("Please fill in all option fields.");
+        return false;
+      }
+      }
+    return true;
+  };
+
+  const saveQuestions = async () => {
+    if (validateQuestions()) {
+      setIsSaving(true);
+      try {
+        const response = await axiosPrivate.post("/quiz/questi", questions);
+        toast.success("Questions saved successfully");
+        setIsAccordionOpen(false);
+      } catch (error) {
+        toast.error("Failed to save questions");
+      } finally {
+        setIsSaving(false);
+        setIsAccordionOpen(false);
+      }
+    }
+  };
+  
+  const [isAccordionOpen, setIsAccordionOpen] = useState(false);
+  const toggleAccordion = () => {
+    setIsAccordionOpen(!isAccordionOpen);
+  };
+
+
+
+  return (
+    <div className="flex justify-center mb-2  ">
+      <div
+        className={`flex flex-col justify-center  ${
+          isAccordionOpen ? "" : "h-[60px]"
+        } bg-base-100  w-full  rounded-lg shadow-xl`}
+      >
+        {/* Heading with background image */}
+
+        <div
+          className="flex justify-between rounded-t-xl mt-5 h-max  w-full  accordion-head cursor-pointer"
+          onClick={toggleAccordion}
+        >
+          <h2 className="font-bold p-5 pb-0 ">
+            <span className="badge badge-neutral  p-3  ">{module_number}</span>{" "}
+            <span className=" italic px-5 "> {module_name} </span>
+          </h2>
+          <span className="pt-6 pr-5">
+            {isAccordionOpen ? <IoIosArrowUp /> : <IoIosArrowDown />}
+          </span>
+        </div>
+
+        {/* Content below the heading */}
+        <div
+          className={`grid grid-cols-1 sm:grid-cols-2 gap-3 m-5 accordion-body  transition-all duration-500 ease-in-out ${
+            isAccordionOpen ? "max-h-full" : "max-h-0 overflow-hidden"
+          }`}
+        >
+          {questions.map((question, index) => (
+            <div
+              key={index}
+              className="w-full flex flex-wrap gap-2 flex-col bg"
+            >
+              <input
+                className="input  input-bordered input-primary w-full"
+                type="text"
+                value={question.question}
+                onChange={(e) =>
+                  updateQuestion(index, "question", e.target.value)
+                }
+                placeholder={`----Type question ${index + 1}--------`}
+              />
+              {
+                question.options.map((option, optionIndex) => (
+                  <div key={optionIndex} className="flex gap-1 items-center">
+                    <input
+                      className={`input input-bordered ${option.is_correct ? 'input-success' : ''} input-sm w-full max-w-xs`}
+                      type="text"
+                      value={option.option_value}
+                      onChange={(e) =>
+                        updateQuestion(index, "options", {
+                          optionIndex,
+                          optionValue: e.target.value,
+                        })
+                      }
+                      placeholder={`Option ${option.option_number}`}
+                    />
+                    <input
+                      type="checkbox"
+                      className="checkbox"
+                      checked={option.is_correct}
+                      onChange={(e) =>
+                        updateQuestion(index, "options", {
+                          optionIndex,
+                          isCorrect: e.target.checked,
+                        })
+                      }
+                    />
+                  </div>
+                ))
+                }
+            </div>
+          ))}
+          <div>
+            <button className="btn btn-outline w-28" onClick={addQuestion}>
+              Add More
+            </button>
+            <button
+              onClick={saveQuestions}
+              className={`btn btn-neutral w-28 ml-2 ${
+                isSaving ? "animate-pulse" : ""
+              }`}
+            >
+              {isSaving ? (
+                <div className="animate-spin rounded-full  w-5 h-5 border-t-2 border-b-3 dark:border-[black] border-[#ffffff]"></div>
+              ) : (
+                "Save"
+              )}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ModuleQuestionReadWrite;
