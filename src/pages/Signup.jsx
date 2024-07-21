cbd27c68dbb26c5b7291b7fc789ea7361a905f99
07SUJITH <tssujith2002@gmail.com>
2024-07-21 11:03:26 +0530

    style:  add icons for input field

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,7 @@
 import { motion } from 'framer-motion'
 import { useEffect, useRef, useState } from 'react'
 import toast from 'react-hot-toast'
+import { MdEmail } from 'react-icons/md'
 import { Link, useNavigate } from 'react-router-dom'
 
 import HomeLayout from '../components/HomeLayout'
@@ -61,10 +62,7 @@ const Signup = () => {
 
         try {
             setLoading(true)
-            const response = await axiosInstance.post(
-                '/auth/register/',
-                signupDetails
-            )
+            await axiosInstance.post('/auth/register/', signupDetails)
             navigate('/signin')
             toast.success(
                 'Account created successfully! Please verify your email.',
@@ -110,44 +108,76 @@ const Signup = () => {
                         <label htmlFor="email" className=" font-bold">
                             Email{' '}
                         </label>
-                        <input
-                            onChange={handleUserInputs}
-                            type="email"
-                            placeholder="Enter your email"
-                            id="email"
-                            name="email"
-                            value={signupDetails.email}
-                            className="input input-bordered w-full max-w-xs"
-                            ref={emailRef}
-                        />
+
+                        <label className="input input-bordered flex items-center gap-2">
+                            <span className="text-gray-400">
+                                <MdEmail />{' '}
+                            </span>
+                            <input
+                                type="text"
+                                className="grow"
+                                placeholder="name23@gmail.com"
+                                onChange={handleUserInputs}
+                                id="email"
+                                name="email"
+                                value={signupDetails.email}
+                                ref={emailRef}
+                            />
+                        </label>
                     </div>
                     <div className="flex flex-col gap-1">
                         <label htmlFor="username" className=" font-bold">
                             username{' '}
                         </label>
-                        <input
-                            onChange={handleUserInputs}
-                            type="text"
-                            placeholder="Enter username"
-                            id="username"
-                            name="username"
-                            value={signupDetails.username}
-                            className="input input-bordered w-full max-w-xs"
-                        />
+
+                        <label className="input input-bordered flex items-center gap-2">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 16 16"
+                                fill="currentColor"
+                                className="w-4 h-4 text-gray-400"
+                            >
+                                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+                            </svg>
+                            <input
+                                type="text"
+                                className="grow"
+                                placeholder="Username"
+                                onChange={handleUserInputs}
+                                id="username"
+                                name="username"
+                                value={signupDetails.username}
+                            />
+                        </label>
                     </div>
                     <div className="flex flex-col gap-1">
                         <label htmlFor="password" className=" font-bold">
                             Password{' '}
                         </label>
-                        <input
-                            onChange={handleUserInputs}
-                            type="password"
-                            placeholder="Enter your password"
-                            id="password"
-                            name="password"
-                            value={signupDetails.password}
-                            className="input input-bordered w-full max-w-xs"
-                        />
+
+                        <label className="input input-bordered flex items-center gap-2">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 16 16"
+                                fill="currentColor"
+                                className="w-4 h-4 text-gray-400"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                                    clipRule="evenodd"
+                                />
+                            </svg>
+                            <input
+                                type="password"
+                                className="grow"
+                                onChange={handleUserInputs}
+                                placeholder="Enter your password"
+                                id="password"
+                                name="password"
+                                value={signupDetails.password}
+                            />
+                        </label>
                     </div>
                     <button className="btn btn-outline btn-active">
                         {loading ? (
